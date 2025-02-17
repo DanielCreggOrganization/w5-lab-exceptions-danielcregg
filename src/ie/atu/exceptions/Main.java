26742f6bd2c85fb13e6000d989f4c7522104df7e
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-02-17 13:38:01 +0000

    Complete Section 1 DIY - Null check implementation

--- src/ie/atu/exceptions/Main.java
+++ src/ie/atu/exceptions/Main.java
@@ -3,5 +3,13 @@ package ie.atu.exceptions;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Exception handling program is running!");
+
+        // Section 1 - Introduction DIY Exercise
+        String text = null;
+        if (text != null) {
+            System.out.println("Text length: " + text.length());
+        } else {
+            System.out.println("Text is null. Cannot retrieve length.");
+        }
     }
 }
